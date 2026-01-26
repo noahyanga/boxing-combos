23e800715ca621c48099dd97545ed56501eb5739
noah <nyanga@rrc.ca>
2026-01-25 18:56:15 -0600

    Update move list

--- src/constants/focuses.js
+++ src/constants/focuses.js
@@ -1,92 +1,137 @@
 import baseWeights from "./baseWeights.js";
 
 const focuses = {
-	none: {
-		name: "None",
-		moves: Object.keys(baseWeights),
-	},
+  none: {
+    name: "None",
+    moves: Object.keys(baseWeights),
+  },
 
-	power: {
-		name: "Power",
-		moves: [
-			"cross",
-			"lead_hook",
-			"rear_hook",
-			"lead_uppercut",
-			"rear_uppercut",
-			"overhand_left",
-			"overhand_right",
-			"hook_uppercut",
-			"bodyshot"
-		],
-	},
+  power: {
+    name: "Power",
+    moves: [
+      "cross",
+      "lead_hook",
+      "rear_hook",
+      "lead_uppercut",
+      "rear_uppercut",
+      "overhand_left",
+      "overhand_right",
+      "hook_uppercut",
+      "bodyshot",
+      "lead_body_hook",
+      "rear_body_hook",
+      "lead_body_shot",
+      "rear_body_shot",
+      "shovel_hook",
+    ],
+  },
 
-	speed: {
-		name: "Speed",
-		moves: [
-			"jab",
-			"double_jab",
-			"jab_cross",     // fast straight combo work
-			"slip",          // quick head movement
-			"shuffle",
-			"step_left",
-			"step_right"
-		],
-	},
+  speed: {
+    name: "Speed",
+    moves: [
+      "jab",
+      "double_jab",
+      "jab_cross", // fast straight combo work
+      "slip", // quick head movement
+      "shuffle",
+      "step_left",
+      "step_right",
+      "jab_feint",
+      "cross_feint",
+      "check_hook",
+    ],
+  },
 
-	defense: {
-		name: "Defense",
-		moves: [
-			"slip",
-			"duck",
-			"pull",
-			"parry",
-			"block",
-			"pivot",
-			"circle"
-		],
-	},
+  defense: {
+    name: "Defense",
+    moves: [
+      "slip",
+      "duck",
+      "pull",
+      "parry",
+      "block",
+      "pivot",
+      "circle",
+      "roll",
+      "slip_counter",
+      "pull_counter",
+      "pivot_counter",
+    ],
+  },
 
-	footwork: {
-		name: "Footwork",
-		moves: [
-			"step_forward",
-			"step_back",
-			"step_left",
-			"step_right",
-			"shuffle",
-			"circle"
-		],
-	},
+  footwork: {
+    name: "Footwork",
+    moves: [
+      "step_forward",
+      "step_back",
+      "step_left",
+      "step_right",
+      "shuffle",
+      "circle",
+    ],
+  },
 
-	// optional extras:
-	counter: {
-		name: "Counter",
-		moves: [
-			"slip",
-			"pull",
-			"parry",
-			"pivot",
-			"jab",
-			"cross",
-			"lead_hook"
-		],
-	},
+  // optional extras:
+  counter: {
+    name: "Counter",
+    moves: [
+      "slip",
+      "pull",
+      "parry",
+      "pivot",
+      "jab",
+      "cross",
+      "lead_hook",
+      "slip_counter",
+      "pull_counter",
+      "pivot_counter",
+      "check_hook",
+      "roll",
+    ],
+  },
 
-	cardio: {
-		name: "Cardio",
-		moves: [
-			"jab",
-			"double_jab",
-			"jab_cross",
-			"shuffle",
-			"step_forward",
-			"step_back",
-			"circle"
-		],
-	},
+  cardio: {
+    name: "Cardio",
+    moves: [
+      "jab",
+      "double_jab",
+      "jab_cross",
+      "shuffle",
+      "step_forward",
+      "step_back",
+      "circle",
+    ],
+  },
+
+  feints: {
+    name: "Feints",
+    moves: [
+      "jab_feint",
+      "cross_feint",
+      "lead_hook_feint",
+      "rear_hook_feint",
+      "lead_uppercut_feint",
+      "rear_uppercut_feint",
+      "jab",
+      "cross",
+      "lead_hook",
+      "rear_hook",
+    ],
+  },
+
+  body_work: {
+    name: "Body Work",
+    moves: [
+      "lead_body_hook",
+      "rear_body_hook",
+      "lead_body_shot",
+      "rear_body_shot",
+      "bodyshot",
+      "shovel_hook",
+      "lead_uppercut",
+      "rear_uppercut",
+    ],
+  },
 };
-
-
 
 export default focuses;
--- src/constants/validMoves.js
+++ src/constants/validMoves.js
@@ -1,231 +1,316 @@
 const validMoves = {
-	jab: [
-		"cross",
-		"lead_hook",
-		"rear_hook",        // add
-		"lead_uppercut",    // add
-		"rear_uppercut",    // add
-		"slip",
-		"pivot",
-		"pull",
-		"jab",
-		"double_jab",
-		"jab_cross"
-	],
+  jab: [
+    "cross",
+    "lead_hook",
+    "rear_hook", // add
+    "lead_uppercut", // add
+    "rear_uppercut", // add
+    "slip",
+    "pivot",
+    "pull",
+    "jab",
+    "double_jab",
+    "jab_cross",
+    "jab_feint",
+    "roll",
+  ],
 
-	double_jab: [
-		"cross",
-		"lead_hook",
-		"rear_hook",        // add
-		"lead_uppercut",    // add
-		"rear_uppercut",    // add
-		"slip",
-		"pivot",
-		"pull",
-		"jab",
-		"jab_cross"
-	],
+  double_jab: [
+    "cross",
+    "lead_hook",
+    "rear_hook", // add
+    "lead_uppercut", // add
+    "rear_uppercut", // add
+    "slip",
+    "pivot",
+    "pull",
+    "jab",
+    "jab_cross",
+    "jab_feint",
+  ],
 
-	cross: [
-		"lead_hook",
-		"rear_hook",        // add
-		"lead_uppercut",
-		"rear_uppercut",    // add
-		"slip",
-		"pivot",
-		"pull",
-		"overhand_left",
-		"bodyshot",
-		"duck",             // add
-		"parry",            // add
-		"jab_cross"
-	],
+  cross: [
+    "lead_hook",
+    "rear_hook", // add
+    "lead_uppercut",
+    "rear_uppercut", // add
+    "slip",
+    "pivot",
+    "pull",
+    "overhand_left",
+    "bodyshot",
+    "duck", // add
+    "parry", // add
+    "jab_cross",
+    "cross_feint",
+    "roll",
+  ],
 
-	jab_cross: [
-		"lead_hook",
-		"rear_hook",        // add
-		"lead_uppercut",
-		"rear_uppercut",    // add
-		"slip",
-		"pivot",
-		"pull",
-		"overhand_left",
-		"bodyshot",
-		"duck",             // add
-		"parry",            // add
-	],
+  jab_cross: [
+    "lead_hook",
+    "rear_hook", // add
+    "lead_uppercut",
+    "rear_uppercut", // add
+    "slip",
+    "pivot",
+    "pull",
+    "overhand_left",
+    "bodyshot",
+    "duck", // add
+    "parry", // add
+    "roll",
+  ],
 
+  lead_hook: [
+    "lead_uppercut",
+    "cross", // add (3–2)
+    "jab",
+    "slip",
+    "pivot",
+    "pull",
+    "hook_uppercut",
+    "bodyshot",
+    "duck", // add
+    "parry", // add
+    "lead_hook_feint",
+    "lead_body_hook",
+    "roll",
+  ],
 
+  rear_hook: [
+    "lead_uppercut",
+    "cross", // add
+    "jab",
+    "slip",
+    "pivot", // add
+    "rear_hook_feint",
+    "rear_body_hook",
+    "roll",
+  ],
 
-	lead_hook: [
-		"lead_uppercut",
-		"cross",            // add (3–2)
-		"jab",
-		"slip",
-		"pivot",
-		"pull",
-		"hook_uppercut",
-		"bodyshot",
-		"duck",             // add
-		"parry"             // add
-	],
+  lead_uppercut: [
+    "jab",
+    "cross", // add (5–2)
+    "slip",
+    "pivot",
+    "pull",
+    "rear_hook",
+    "hook_uppercut",
+    "lead_uppercut_feint",
+    "roll",
+  ],
 
-	rear_hook: [
-		"lead_uppercut",
-		"cross",            // add
-		"jab",
-		"slip",
-		"pivot"             // add
-	],
+  rear_uppercut: [
+    "jab",
+    "cross", // add (6–2)
+    "slip",
+    "pivot",
+    "pull",
+    "lead_hook",
+    "hook_uppercut",
+    "rear_uppercut_feint",
+    "roll",
+  ],
 
-	lead_uppercut: [
-		"jab",
-		"cross",            // add (5–2)
-		"slip",
-		"pivot",
-		"pull",
-		"rear_hook",
-		"hook_uppercut"
-	],
+  slip: [
+    "jab",
+    "cross",
+    "lead_hook",
+    "pivot",
+    "pull",
+    "duck",
+    "parry",
+    "block",
+    "roll",
+    "slip_counter",
+  ],
 
-	rear_uppercut: [
-		"jab",
-		"cross",            // add (6–2)
-		"slip",
-		"pivot",
-		"pull",
-		"lead_hook",
-		"hook_uppercut"
-	],
+  pivot: [
+    "jab",
+    "cross",
+    "step_forward",
+    "step_back",
+    "step_left",
+    "step_right",
+    "roll",
+    "pivot_counter",
+  ],
 
-	slip: [
-		"jab",
-		"cross",
-		"lead_hook",
-		"pivot",
-		"pull",
-		"duck",
-		"parry",
-		"block"
-	],
+  pull: ["jab", "cross", "step_back", "circle", "pull_counter", "roll"],
 
-	pivot: [
-		"jab",
-		"cross",
-		"step_forward",
-		"step_back",
-		"step_left",
-		"step_right"
-	],
+  overhand_left: [
+    "jab",
+    "lead_hook",
+    "lead_uppercut",
+    "bodyshot",
+    "slip", // add
+    "duck", // add
+    "pivot", // add
+  ],
 
-	pull: [
-		"jab",
-		"cross",
-		"step_back",
-		"circle"
-	],
+  overhand_right: [
+    "jab",
+    "rear_hook",
+    "rear_uppercut",
+    "bodyshot",
+    "slip", // add
+    "duck", // add
+    "pivot", // add
+  ],
 
-	overhand_left: [
-		"jab",
-		"lead_hook",
-		"lead_uppercut",
-		"bodyshot",
-		"slip",             // add
-		"duck",             // add
-		"pivot"             // add
-	],
+  bodyshot: [
+    "slip",
+    "duck",
+    "pivot",
+    "step_back", // add
+    "circle", // add
+  ],
 
-	overhand_right: [
-		"jab",
-		"rear_hook",
-		"rear_uppercut",
-		"bodyshot",
-		"slip",             // add
-		"duck",             // add
-		"pivot"             // add
-	],
+  double_jab: ["cross", "lead_hook", "slip", "jab_cross"],
 
-	bodyshot: [
-		"slip",
-		"duck",
-		"pivot",
-		"step_back",        // add
-		"circle"            // add
-	],
+  duck: ["jab", "cross", "lead_uppercut", "pivot"],
 
-	double_jab: [
-		"cross",
-		"lead_hook",
-		"slip",
-		"jab_cross"
-	],
+  parry: ["jab", "cross", "lead_hook"],
 
-	duck: [
-		"jab",
-		"cross",
-		"lead_uppercut",
-		"pivot"
-	],
+  block: ["jab", "cross", "lead_hook", "slip"],
 
-	parry: [
-		"jab",
-		"cross",
-		"lead_hook"
-	],
+  step_forward: [
+    "jab",
+    "cross",
+    "lead_hook",
+    "shuffle", // add
+    "circle", // add
+  ],
 
-	block: [
-		"jab",
-		"cross",
-		"lead_hook",
-		"slip"
-	],
+  step_back: [
+    "slip",
+    "pivot",
+    "pull",
+    "shuffle", // add
+    "circle", // add
+  ],
 
-	step_forward: [
-		"jab",
-		"cross",
-		"lead_hook",
-		"shuffle",          // add
-		"circle"            // add
-	],
+  step_left: [
+    "jab",
+    "cross",
+    "slip",
+    "shuffle", // add
+  ],
 
-	step_back: [
-		"slip",
-		"pivot",
-		"pull",
-		"shuffle",          // add
-		"circle"            // add
-	],
+  step_right: [
+    "jab",
+    "cross",
+    "slip",
+    "shuffle", // add
+  ],
 
-	step_left: [
-		"jab",
-		"cross",
-		"slip",
-		"shuffle"           // add
-	],
+  shuffle: [
+    "jab",
+    "cross",
+    "lead_hook",
+    "step_forward", // add
+    "step_back", // add
+  ],
 
-	step_right: [
-		"jab",
-		"cross",
-		"slip",
-		"shuffle"           // add
-	],
+  circle: [
+    "jab",
+    "cross",
+    "slip",
+    "pivot", // add
+    "circle", // add
+  ],
 
-	shuffle: [
-		"jab",
-		"cross",
-		"lead_hook",
-		"step_forward",     // add
-		"step_back"         // add
-	],
+  // Feints
+  jab_feint: [
+    "cross",
+    "lead_hook",
+    "lead_uppercut",
+    "rear_hook",
+    "rear_uppercut",
+    "jab",
+  ],
 
-	circle: [
-		"jab",
-		"cross",
-		"slip",
-		"pivot",            // add
-		"circle"            // add
-	]
+  cross_feint: ["jab", "lead_hook", "lead_uppercut", "cross"],
+
+  lead_hook_feint: ["jab", "cross", "lead_uppercut", "rear_hook", "lead_hook"],
+
+  rear_hook_feint: ["jab", "cross", "rear_uppercut", "lead_hook", "rear_hook"],
+
+  lead_uppercut_feint: [
+    "jab",
+    "cross",
+    "lead_hook",
+    "rear_hook",
+    "lead_uppercut",
+  ],
+
+  rear_uppercut_feint: [
+    "jab",
+    "cross",
+    "lead_hook",
+    "rear_hook",
+    "rear_uppercut",
+  ],
+
+  // Body shots
+  lead_body_hook: [
+    "lead_uppercut",
+    "rear_hook",
+    "slip",
+    "duck",
+    "pivot",
+    "step_back",
+  ],
+
+  rear_body_hook: [
+    "lead_hook",
+    "lead_uppercut",
+    "slip",
+    "duck",
+    "pivot",
+    "step_back",
+  ],
+
+  lead_body_shot: ["lead_uppercut", "cross", "slip", "duck", "pivot"],
+
+  rear_body_shot: ["lead_hook", "jab", "slip", "duck", "pivot"],
+
+  // Defensive moves
+  roll: [
+    "jab",
+    "cross",
+    "lead_hook",
+    "rear_hook",
+    "lead_uppercut",
+    "rear_uppercut",
+    "slip",
+    "duck",
+  ],
+
+  slip_counter: [
+    "cross",
+    "lead_hook",
+    "rear_hook",
+    "lead_uppercut",
+    "rear_uppercut",
+  ],
+
+  pull_counter: [
+    "jab",
+    "cross",
+    "lead_hook",
+    "overhand_left",
+    "overhand_right",
+  ],
+
+  pivot_counter: ["lead_hook", "rear_hook", "lead_uppercut", "overhand_left"],
+
+  // Advanced combinations
+  hook_uppercut: ["cross", "rear_hook", "slip", "pivot", "duck"],
+
+  check_hook: ["jab", "cross", "slip", "pivot", "step_back"],
+
+  shovel_hook: ["lead_uppercut", "rear_uppercut", "slip", "duck", "pivot"],
 };
 
 export default validMoves;
-
